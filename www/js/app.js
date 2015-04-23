81300531d425d1c2a70a13e1aa0c3ef6891ed11b
Sumeet Pareek <positivecharge@gmail.com>
2015-04-23 15:36:24 -0400

    Minor changes to have a separate start page

--- www/js/app.js
+++ www/js/app.js
@@ -29,6 +29,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     templateUrl: "templates/menu.html",
     controller: 'AppCtrl'
   })
+  
+  .state('app.startpage', {
+    url: "/startpage",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/startpage.html"
+      }
+    }
+  })
 
   .state('app.search', {
     url: "/search",
@@ -67,5 +76,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/startpage');
 });
